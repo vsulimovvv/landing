8c23d99f0941a4b11538d0b45c1b400087c7e087
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-13 19:35:59 +0300

    slider is over

--- script/script.js
+++ script/script.js
@@ -178,8 +178,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const li = document.createElement('li');
         if (i === 0) {
           li.className = 'dot dot-active';
-        }
-        else {
+        } else {
           li.className = 'dot';
         }
         ul.appendChild(li);
@@ -202,7 +201,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const autoPlaySlide = () => {
 
-      prevSlide(slides, currentSlide, 'zportfolio-item-active');
+      prevSlide(slides, currentSlide, 'portfolio-item-active');
       prevSlide(dot, currentSlide, 'dot-active');
       currentSlide++;
       if (currentSlide >= slides.length) {
@@ -272,8 +271,72 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    startSlide();
+    startSlide(1000);
   }
 
   slider();
+
+
+  // changeImage
+  // У каждой фото есть data атрибут с другой картинкой data-image.
+  // Необходимо реализовать, чтобы по наведению мышкой менялись фотографии, а если увести мышку с элемента то возвращается прежняя фото.
+
+  // slider.addEventListener('mouseover', event => {
+  //   if (
+  //     event.target.matches('.portfolio-btn') ||
+  //     event.target.matches('.dot')
+  //   ) {
+  //     stopSlide();
+  //   }
+  // });
+
+  // slider.addEventListener('mouseout', event => {
+  //   if (
+  //     event.target.matches('.portfolio-btn') ||
+  //     event.target.matches('.dot')
+  //   ) {
+  //     startSlide();
+  //   }
+  // });
+
+
+  // Вешаешь обработчик событие наведение мыши, с таргета выцепляешь дефолтный путь который в src и сохраняешь в переменную, потом ссылку в src меняешь путем из data. В этом же обработчике навешиваешь событие убрать мышь и в нем ставишь в src ссылку которая была изначально
+
+  // const changeImage = () => {
+
+  //   const commandPhoto = document.querySelectorAll('.command__photo');
+
+  //   commandPhoto.forEach((item) => {
+
+  //     item.addEventListener('mouseenter', event => {
+
+  //       let defaultPath = event.target.getAttribute('src');
+  //       event.target.setAttribute((event.target.getAttribute(defaultPath)));
+  //       // event.target.setAttribute((event.target.getAttribute('data-img')));
+  //       // event.target.src = event.target.dataset.img;
+  //       // console.log(defaultPath);
+
+  //     });
+
+  //     item.addEventListener('mouseout', event => {
+
+
+  //     });
+  //   });
+  // }
+
+  // changeImage();
+
+  // ввод только чисел
+  const inputNumber = () => {
+
+    inputTypeNumber = document.querySelectorAll('input[type=number]');
+
+    inputTypeNumber.forEach((item) => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/[^0-9]/, '');
+      });
+    })
+    inputNumber();
+  }
 });
