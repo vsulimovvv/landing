a8277fa9d39dab6e35a60a171d30375db168003b
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-18 16:45:57 +0300

    lesson22

--- script/script.js
+++ script/script.js
@@ -304,4 +304,46 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
   inputNumber();
+
+  // calculate 
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.querySelector('#total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      } 
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', event => {
+      const target = event.target;
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  }
+  calc(100);
 });
