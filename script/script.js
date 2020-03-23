81166e7ff0c17102120e3d88da9d270573485294
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-23 21:16:22 +0300

    lesson25

--- script/script.js
+++ script/script.js
@@ -348,36 +348,36 @@ document.addEventListener("DOMContentLoaded", () => {
   calc(100);
 
   // send-ajax-form
-  // const inputTypeTel = () => {
-  //   const inputTypeTel = document.querySelectorAll('input[type=tel]');
-  //   inputTypeTel.forEach((item) => {
-  //     item.addEventListener('input', () => {
-  //       item.value = item.value.replace(/[^\d+]/g, '');
-  //     });
-  //   });
-  // }
-  // inputTypeTel();
+  const inputTypeTel = () => {
+    const inputTypeTel = document.querySelectorAll('input[type=tel]');
+    inputTypeTel.forEach((item) => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/[^\d+]/g, '');
+      });
+    });
+  }
+  inputTypeTel();
 
-  // const inputForm1 = () => {
-  //   const form1message = document.querySelector('#form1-name');
-  //   form1message.addEventListener('input', () => {
-  //     form1message.value = form1message.value.replace(/[^а-яА-Я]/, '');
-  //   });
-  // }
-  // inputForm1();
+  const inputForm1 = () => {
+    const form1message = document.querySelector('#form1-name');
+    form1message.addEventListener('input', () => {
+      form1message.value = form1message.value.replace(/[^а-яА-Я]/, '');
+    });
+  }
+  inputForm1();
 
-  // const inputForm2 = () => {
-  //   const form2Message = document.querySelector('#form2-message');
-  //   const form2Name = document.querySelector('#form2-name');
-  //   form2Message.addEventListener('input', () => {
-  //     form2Message.value = form2Message.value.replace(/[^а-яА-Я]/, '');
-  //   });
-  //   form2Name.addEventListener('input', () => {
-  //     form2Name.value = form2Name.value.replace(/[^а-яА-Я]/, '');
-  //   });
-  // }
+  const inputForm2 = () => {
+    const form2Message = document.querySelector('#form2-message');
+    const form2Name = document.querySelector('#form2-name');
+    form2Message.addEventListener('input', () => {
+      form2Message.value = form2Message.value.replace(/[^а-яА-Я]/, '');
+    });
+    form2Name.addEventListener('input', () => {
+      form2Name.value = form2Name.value.replace(/[^а-яА-Я]/, '');
+    });
+  }
 
-  // inputForm2();
+  inputForm2();
 
   const sendForm = () => {
 
